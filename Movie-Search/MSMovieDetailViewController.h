a117f8bc7dbbea0dff7ba76b8e7ae1d0c212f336
Jason Choi <jasonnoahchoi@gmail.com>
2015-03-03 18:04:16 -0700

    Create a detailviewcontroller that displays the results of the get call

--- Movie-Search/MSMovieDetailViewController.h
+++ Movie-Search/MSMovieDetailViewController.h
@@ -10,4 +10,6 @@
 
 @interface MSMovieDetailViewController : UIViewController
 
+
+
 @end
--- Movie-Search/MSMovieDetailViewController.m
+++ Movie-Search/MSMovieDetailViewController.m
@@ -7,18 +7,26 @@
 //
 
 #import "MSMovieDetailViewController.h"
+#import "MovieController.h"
+#import "MSViewController.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
 
 @interface MSMovieDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterImageView;
 
 @end
 
 @implementation MSMovieDetailViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+- (instancetype)init {
+    self = [super init];
     if (self) {
-        // Custom initialization
+
     }
     return self;
 }
@@ -26,6 +34,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    NSDictionary *movieSelection = [MovieController sharedInstance].dictionary;
+    NSLog(@"%@", movieSelection);
+
+//    MSViewController *vc = [[MSViewController alloc] init];
+//
+//    [[MovieController sharedInstance] retrieveMovieByID:vc.movieID completion:^(BOOL success, NSDictionary *dictionary) {
+    self.titleLabel.text = [movieSelection objectForKey:@"original_title"];
+    NSLog(@"%@", self.titleLabel.text);
+    self.synopsisLabel.text = [movieSelection objectForKey:@"overview"];
+    self.ratingLabel.text = [NSString stringWithFormat:@"%@", [movieSelection objectForKey:@"vote_average"]];
+    self.yearLabel.text = [NSString stringWithFormat:@"%@",[movieSelection objectForKey:@"release_date"]];
+    [self.posterImageView setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://image.tmdb.org/t/p/w92%@", [movieSelection objectForKey:@"poster_path"]]]];
+//    }];
+
+
     // Do any additional setup after loading the view from its nib.
 }
 
